e66d5ae2e31e352a82e5771fee57b6d69fe558ac
Dev-KimDonghyun <hyungus723@gmail.com>
2025-02-09 11:00:14 +0900

    fix: console에 표출된 오류 수정 - CommonJS 오류 수정 - 일부 코드 개선

--- bringnews/src/api/api.js
+++ bringnews/src/api/api.js
@@ -1,9 +1,11 @@
 import { API_Key } from './keys.js';
+import fetch from 'node-fetch'; // CommonJS 충돌 해결 by ChatGPT
 
 const newsLocal = 'jp';
 const newsNumber = '1';
+const newsLanguage = 'ja';
 
-const bringNewsUrl = `https://api.thenewsapi.com/v1/news/top?api_token=${API_Key}&locale=${newsLocal}&limit=${newsNumber}`;
+const bringNewsUrl = `https://api.thenewsapi.com/v1/news/top?api_token=${API_Key}&locale=${newsLocal}&limit=${newsNumber}&language=${newsLanguage}`; //API URL 상세정보 (뉴스 언어) 추가
 
 const requestOptions = {
     method: 'GET'
@@ -13,5 +15,7 @@ const requestOptions = {
 
 fetch(bringNewsUrl, requestOptions)
     .then(response => response.text())
-    .then(result => console.log(result))
+    .then(result => {
+        console.log(result);
+    })
     .catch(error => console.log('error', error));
